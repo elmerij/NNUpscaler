6521e82c9249497b9e1c1b5473e9c270b3dd0c35
Elmeri <80104775+elmerij@users.noreply.github.com>
2022-02-12 13:48:00 +0200

    Missing packages folder, install these nuget packages: DeepAI.Client, SipaaFramework

---
+++ NNUpscaler/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace NNUpscaler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sButton1 = new Sipaa.Framework.SButton();
+            this.sButton2 = new Sipaa.Framework.SButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectedfile = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sButton1
+            // 
+            this.sButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.sButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.sButton1.BorderRadius = 6;
+            this.sButton1.BorderSize = 0;
+            this.sButton1.FlatAppearance.BorderSize = 0;
+            this.sButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sButton1.ForeColor = System.Drawing.Color.White;
+            this.sButton1.Location = new System.Drawing.Point(15, 71);
+            this.sButton1.Name = "sButton1";
+            this.sButton1.Size = new System.Drawing.Size(150, 40);
+            this.sButton1.TabIndex = 0;
+            this.sButton1.Text = "Upscale";
+            this.sButton1.UseVisualStyleBackColor = false;
+            this.sButton1.Click += new System.EventHandler(this.sButton1_Click);
+            // 
+            // sButton2
+            // 
+            this.sButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.sButton2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.sButton2.BorderRadius = 6;
+            this.sButton2.BorderSize = 0;
+            this.sButton2.FlatAppearance.BorderSize = 0;
+            this.sButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sButton2.ForeColor = System.Drawing.Color.White;
+            this.sButton2.Location = new System.Drawing.Point(15, 12);
+            this.sButton2.Name = "sButton2";
+            this.sButton2.Size = new System.Drawing.Size(150, 40);
+            this.sButton2.TabIndex = 1;
+            this.sButton2.Text = "Select File";
+            this.sButton2.UseVisualStyleBackColor = false;
+            this.sButton2.Click += new System.EventHandler(this.sButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selected File:";
+            // 
+            // selectedfile
+            // 
+            this.selectedfile.AutoSize = true;
+            this.selectedfile.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectedfile.ForeColor = System.Drawing.Color.White;
+            this.selectedfile.Location = new System.Drawing.Point(117, 55);
+            this.selectedfile.Name = "selectedfile";
+            this.selectedfile.Size = new System.Drawing.Size(0, 14);
+            this.selectedfile.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(592, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(15, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(179, 149);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.selectedfile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sButton2);
+            this.Controls.Add(this.sButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "NNUpscaler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Sipaa.Framework.SButton sButton1;
+        private Sipaa.Framework.SButton sButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label selectedfile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ NNUpscaler/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using DeepAI; // Add this line to the top of your file
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NNUpscaler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sButton1_Click(object sender, EventArgs e)
+        {
+            DeepAI_API api = new DeepAI_API(apiKey: "f324a1e7-c056-4613-9e27-9cfb0ccad096");
+
+            StandardApiResponse resp = api.callStandardApi("torch-srgan", new
+            {
+                image = File.OpenRead(label2.Text),
+            });
+            textBox1.Text=api.objectAsJsonString(resp);
+        }
+        
+        private void sButton2_Click(object sender, EventArgs e)
+        {
+            string pathfull = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filepath = openFileDialog.FileName;
+                pathfull = filepath;
+            }
+            string extractedfilename = null;
+
+            // using the method
+            extractedfilename = Path.GetFileName(pathfull);
+            selectedfile.Text= extractedfilename;
+            label2.Text = pathfull;
+        }
+    }
+}
